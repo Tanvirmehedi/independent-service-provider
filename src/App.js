ff644deefa0c0279864ca757789b7e38c35a8f9c
Tanvirmehedi <tjmehedi201@gmail.com>
2022-04-17 01:44:43 +0600

    responsive navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./component/Footer/Footer";
+import Header from "./component/Header/Header";
+import Home from "./component/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Home</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/component/Header/Header.js
@@ -0,0 +1,41 @@
+import { MenuIcon, XIcon } from "@heroicons/react/solid";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../image/logo.png";
+import CustomLink from "../CustomLink/CustomLink";
+import "./Header.css";
+const Header = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <header>
+      <nav className="container mx-auto">
+        <div className=" flex justify-between items-center">
+          <div className="logo w-[20%] md:w-[10%]">
+            <Link className="p-0" to="/">
+              <img src={logo} alt="logo" />
+            </Link>
+          </div>
+          <div
+            className={`md:flex md:relative ${
+              !open ? "hidden" : "absolute top-14 bg-orange-100 w-full py-5"
+            }`}
+          >
+            <CustomLink to="/">Home</CustomLink>
+            <CustomLink to="/service">Service</CustomLink>
+            <CustomLink to="/blog">Blog</CustomLink>
+            <CustomLink to="/about">About</CustomLink>
+          </div>
+
+          <div
+            className="hamburger-menu md:hidden mr-5"
+            onClick={() => setOpen(!open)}
+          >
+            {!open ? <MenuIcon height={30} /> : <XIcon height={30} />}
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
