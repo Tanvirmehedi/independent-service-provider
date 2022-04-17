4eff5039d4784e854adc9c33f6a631d444a89381
Tanvirmehedi <tjmehedi201@gmail.com>
2022-04-18 03:52:16 +0600

    Blog page is Done

--- src/component/Blog/DisplayBlog.js
+++ src/component/Blog/DisplayBlog.js
@@ -6,7 +6,7 @@ const DisplayBlog = ({ data }) => {
     <div className="border mx-3 my-2 rounded-sm p-3">
       <h3 className="text-lg py-3 rounded-md bg-slate-100">{question}</h3>
       <hr />
-      <p className="my-5">{answer}</p>
+      <p className="my-5 overflow-hidden">{answer}</p>
     </div>
   );
 };
